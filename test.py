8681a2bb9a6fcfdb8464ff192ed7aa6265ed5d42
aav-antonov <aav.antonov@gmail.com>
2025-08-15 14:42:57 +0100

    votzexuyny

--- test.py
+++ test.py
@@ -96,14 +96,14 @@ def run_dea_comparison(m, fX_k, fY_k):
 
 # Example of how to call the function:
 execution_time_all = []
-m = 1000
+m = 500
 fX_k = 5
 fY_k = 3
-for k in range(1, 4):
+for k in range(1, 6):
     
     print(f"Running DEA comparison for m={m}, fX_k={fX_k}, fY_k={fY_k}")
     
-    if k < 3:   # Run the DEA comparison
+    if k < 4:   # Run the DEA comparison
         etime = run_dea_comparison(m=m, fX_k=5, fY_k=3)
         execution_time_all.append([f"m={m}, fX_k={fX_k}, fY_k={fY_k}",etime])
     else:  # Run the DeaLargeScale
@@ -115,73 +115,6 @@ for [setsize, execution_time] in execution_time_all:
     for key, value in execution_time.items():
         print(f"{setsize} {key}: {value:.4f} seconds")
 
-exit()
 
-# print(times) 
- 
-  
-m = 100
-fX_k = 5
-fY_k = 3
-
-fileX = f'mydata/X{m}_{fX_k}_{fY_k}.npy'
-fileY = f'mydata/Y{m}_{fX_k}_{fY_k}.npy'
-
-X,Y = generateXY(m,fX_k,fY_k, fileX, fileY)
-
-X = np.load(fileX)
-Y = np.load(fileY)
-
-execution_time = compare_dea_methods(X, Y)
-
-for key, value in execution_time.items():
-    print(f"{key}: {value:.4f} seconds")
-
-exit()
-
-############################
-DEAMP = DeaMultiprocessing(THREAD_N = 8)
-DEAMP.set_DEA(X, Y, q_type ="x")
-
-
-start_time = timeit.default_timer()
-qX1 = DEAMP.run(X, Y, q_type="x")
-elapsed_time = timeit.default_timer() - start_time
-
-execution_time = {}
-execution_time['DeaMultiprocessing'] = elapsed_time
-
-print(f"Elapsed time: {elapsed_time:.4f} seconds")
-qX1 = np.array(qX1)
-
-# Run DeaLargeScale
-
-DEALS = DeaLargeScale(THREAD_N = 8)
-start_time = timeit.default_timer()
-qX2 = DEALS.run(X, Y, q_type ="x",steps = 5, size = 100)
-elapsed_time = timeit.default_timer() - start_time
-execution_time['DeaLargeScale'] = elapsed_time
-
-
-qX2 = np.array(qX2)
-
-for key, value in execution_time.items():
-    print(f"{key}: {value:.4f} seconds")
-
-
-
-
-
-
-assert np.allclose(qX1, qX2, atol=1e-8), "Results from DeaMultiprocessing and DeaLargeScale do not match!"
-
-print("Both methods returned the same results.")
-
-print(qX1[:10])
-print(qX2[:10])
-
-
-
-    
     
 

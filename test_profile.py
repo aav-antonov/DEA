43a02865f4cf9db42e8b70f8c7b6d9ab0bec3c81
aav-antonov <aav.antonov@gmail.com>
2025-08-31 08:43:54 +0100

    modify profile

--- test_profile.py
+++ test_profile.py
@@ -24,21 +24,28 @@ def generateXY(m,fX_k,fY_k, fileX, fileY):
     
     return X,Y
 
+#generate random X and Y
+m = 250 #number of DMUs
+fX_k = 5 #size of resouce space (inputs of DMU) )
+fY_k = 3 #size of product space (outputs of DMU) )
 X, Y = generateXY(m, fX_k, fY_k, fileX, fileY)
 
+#init class Profile class and set X and Y
 DP = DeaProfile()
-DP.get_base(X, Y,  q_type ="x", steps = 10, size = 100)
+DP.get_base(X, Y,  q_type ="x")
 
 
-x, y = X[:,5], Y[:,5]*0.2
-DP.get_yx_profile( x, y )
+#select a DMU to profile (in this case with index 5)
+x, y = X[:,5], Y[:,5]*0.2  # also scale output to make it inefficient
 
-DP.get_xx_profile( x, y , 0, 1)
-DP.get_xx_profile( x, y , 1, 2)
-DP.get_xx_profile( x, y , 0, 2)
+#Example of plotting y(x) profile
+DP.get_yx_profile( x, y, file_output = "plot_yx.png" )
 
-# Example usage:
-# X, Y = generate_base_XY(fX_k=3, fY_k=2, scale_range=range(1,11), N=1, bias=0.0)
+#Example of plotting x(x) profile
+DP.get_xx_profile( x, y , 0, 1, file_output = "plot_xx") # see plot_xx_0_1.png
+DP.get_xx_profile( x, y , 1, 2, file_output = "plot_xx") # see plot_xx_1_2.png
+DP.get_xx_profile( x, y , 0, 2, file_output = "plot_xx") # see plot_xx_0_2.png
+
     
     
 
